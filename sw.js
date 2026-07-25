3939252bfc76372203a43269c174f8a52f1cfe88
icarus52-alec <icarus52@gmail.com>
2026-07-26 01:27:51 +0800

    Add count-value warning, PWA support (manifest, icons, service worker)

---
+++ sw.js
@@ -0,0 +1,34 @@
+var CACHE_NAME = "e5-calc-v1";
+var ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event){
+  event.respondWith(
+    fetch(event.request).then(function(res){
+      var resClone = res.clone();
+      caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, resClone); });
+      return res;
+    }).catch(function(){
+      return caches.match(event.request).then(function(cached){
+        return cached || caches.match("./index.html");
+      });
+    })
+  );
+});
